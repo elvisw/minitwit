9dd8a6472b4e2fa894547cd7d19ba5ae96fce514
Elvis Wang <me@elvisw.com>
2015-04-22 22:09:57 +0800

    +  filters detached

--- app/__init__.py
+++ app/__init__.py
@@ -8,9 +8,6 @@
     :copyright: (c) 2010 by Armin Ronacher.
     :license: BSD, see LICENSE for more details.
 """
-
-from hashlib import md5
-from datetime import datetime
 from flask import Flask, session, g
 
 # create our little application :)
@@ -18,25 +15,7 @@ app = Flask(__name__)
 app.config.from_object('config')
 app.config.from_envvar('MINITWIT_SETTINGS', silent=True)
 
-def format_datetime(timestamp):
-    """Format a timestamp for display."""
-    return datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d @ %H:%M')
-
-
-def gravatar_url(email, size=80):
-    """Return the gravatar image for the given email address."""
-    return 'http://www.gravatar.com/avatar/%s?d=identicon&s=%d' % \
-        (md5(email.strip().lower().encode('utf-8')).hexdigest(), size)
-
-# add some filters to jinja
-app.jinja_env.filters['datetimeformat'] = format_datetime
-app.jinja_env.filters['gravatar'] = gravatar_url
-
-from app import views, db
-
-if __name__ == '__main__':
-    db.init_db()
-    app.run()
+from app import views, db, filters
 
 @app.before_request
 def before_request():
@@ -44,3 +23,8 @@ def before_request():
     if 'user_id' in session:
         g.user = db.query_db('select * from user where user_id = ?',
                           [session['user_id']], one=True)
+
+
+if __name__ == '__main__':
+    db.init_db()
+    app.run()
---
+++ app/filters.py
@@ -0,0 +1,18 @@
+from hashlib import md5
+from datetime import datetime
+
+from app import app
+
+def format_datetime(timestamp):
+    """Format a timestamp for display."""
+    return datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d @ %H:%M')
+
+
+def gravatar_url(email, size=80):
+    """Return the gravatar image for the given email address."""
+    return 'http://www.gravatar.com/avatar/%s?d=identicon&s=%d' % \
+        (md5(email.strip().lower().encode('utf-8')).hexdigest(), size)
+
+# add some filters to jinja
+app.jinja_env.filters['datetimeformat'] = format_datetime
+app.jinja_env.filters['gravatar'] = gravatar_url
